536fa28cfcee46754b2d0804d3f3e29832f98f0e
Sascha Schmalz <sassc662@student.liu.se>
2017-03-16 09:31:04 +0100

    Added code for acceptance rejection model

---
+++ cheat sheet stuff/R/acceptance_rejection.R
@@ -0,0 +1,58 @@
+# Template for the acceptance rejection method:
+
+dtarget <- function(x) {
+  dbeta(x, 1, 2)
+}
+
+dproposal <- function(x) {
+  dunif(x)
+}
+
+rproposal <- function() {
+  runif(1)
+}
+
+maxiter <- 2000
+c <- 2
+
+acceptance_rejection <- function(maxiter, c, dt, dp, rp) {
+  # Draws from the target distribution by applying acceptance
+  # rejection model
+  # Args:
+  #   maxiter   No. of elements drawn
+  #   c         majorizing constant
+  #   dt        target density distribution 
+  #   dp        proposal density distribution
+  #   rp        proposal random draw
+  
+  X <- vector("numeric", length= maxiter)
+  i <- 0
+  counter <- 0
+  
+  repeat {
+    Y <- rproposal()
+    U <- runif(1)
+    
+    if (U <= (dtarget(Y) / (c * dproposal(Y)))) {
+      X[i] <- Y
+      i = i + 1
+    }
+    
+    counter = counter +1
+    
+    if (i == maxiter) {break}
+  }
+  
+  return(structure(list(
+    expected_rejection = (c-1)/c,
+    real_rejection = 1 - maxiter/counter,
+    X = X
+  )))
+}
+
+test <- acceptance_rejection(maxiter, c, dt, dp, rp)
+
+hist(test$X)
+
+# beta <- rbeta(1000, 1, 2)
+# hist(beta)
